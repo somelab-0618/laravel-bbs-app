7a315e6f6f1357a3a3f01f5c71628923d1927c1e
RyosukeSomeya <rx.0.0618@gmail.com>
2022-03-13 00:27:22 +0900

    Postモデル関連作成

---
+++ develop/bbs-app/database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->realText(rand(20,50)),
+            'content' => $this->faker->realText(rand(100,140)),
+        ];
+    }
+}
